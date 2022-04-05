1edd5586c875db6441fd883b53d872d5b413fa22
WazmaRas <wazma.rasouli@live.se>
2022-04-05 11:45:27 +0200

    added login feature

--- CodeAid.UI/Pages/Login.cshtml.cs
+++ CodeAid.UI/Pages/Login.cshtml.cs
@@ -1,12 +1,45 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodeAid.UI.Pages
 {
+    [BindProperties]
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
         {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var identityResult = await signInManager.PasswordSignInAsync(Username, Password, false, false);
+                if (identityResult.Succeeded)
+                {
+
+                    return RedirectToPage("/Index");
+
+
+                }
+                ModelState.AddModelError("", "Username or Password incorrect");
+
+            }
+            return Page();
         }
     }
 }
