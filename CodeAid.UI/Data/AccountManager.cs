ebabe3e283a86c466b8a6dbee56e3e25f1a39953
Chiltrin <s.s.ilic@hotmail.com>
2022-04-15 15:36:58 +0200

    Added to AccountManager

--- CodeAid.UI/Data/AccountManager.cs
+++ CodeAid.UI/Data/AccountManager.cs
@@ -1,4 +1,6 @@
-﻿namespace CodeAid.UI.Data
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CodeAid.UI.Data
 {
     public class AccountManager
     {
@@ -18,5 +20,31 @@
             }
             return false;
         }
+
+        public async Task DeleteUser(IdentityUser userDto)
+        {
+            var id = userDto.Id;
+
+            ApiManager apiManager = new();
+            await apiManager.DeleteAccount(id);
+        }
+
+        public async Task DeactiveUser(string id)
+        {
+            ApiManager apiManager = new();
+            await apiManager.DeactivateAccount(id);
+        }
+
+        public async Task<bool> ActivateUser(string id)
+        {
+            ApiManager apiManager = new();
+            var result = await apiManager.ActivateAccount(id);
+            if (result != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
